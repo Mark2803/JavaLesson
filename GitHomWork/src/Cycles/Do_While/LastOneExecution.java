9714b96b9b2b858641649e00812873da820ae766
Mark2803 <110024584+Mark2803@users.noreply.github.com>
2024-12-18 23:50:34 +0700

    add gomework

---
+++ GitHomWork/src/Cycles/Do_While/LastOneExecution.java
@@ -0,0 +1,25 @@
+/*
+Напишите программу, которая запрашивает у пользователя число и выводит
+его удвоенное значение. Цикл должен выполняться до тех пор,
+пока пользователь не введет число больше 10.
+ */
+
+package Cycles.Do_While;
+import java.util.Scanner;
+
+public class LastOneExecution {
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+
+            int num;
+            do {
+                System.out.println("Введите число");
+                num = in.nextInt();
+                System.out.println(num * 2);
+            } while (num <= 10);
+
+        }
+
+    }
+
---
+++ GitHomWork/src/Cycles/While/SumNum500.java
@@ -0,0 +1,21 @@
+/*
+Напишите программу, которая печатает все числа от 1 до 100,
+пока сумма напечатанных чисел не станет больше 500.
+ */
+package Cycles.While;
+
+public class SumNum500 {
+    public static void main(String[] args) {
+
+        int num = 0;
+        int tmp = num;
+        while (tmp <= 500) {
+            num++;
+            tmp = tmp + num;
+
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        System.out.print(tmp);
+    }
+}
